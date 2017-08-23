1dab7684b4a4fe225f437e80f504880f78dd863e
Shivan Modha <shivanmodha@gmail.com>
2017-08-23 00:39:24 -0400

    Stat Test

--- stats.py
+++ stats.py
@@ -7,4 +7,15 @@ except:
     import psutil
     print("psutil installed!")
 
-print(psutil.virtual_memory()[2])
+memory_percentage = psutil.virtual_memory()[2]
+cpu_usage = psutil.cpu_percent(interval=1)
+print (memory_percentage)
+print (cpu_usage)
+print (psutil.cpu_count())
+
+print (psutil.disk_partitions())
+print (psutil.disk_usage('/'))
+
+print (psutil.net_if_addrs())
+
+print (psutil.sensors_temperatures(fahrenheit=True))
